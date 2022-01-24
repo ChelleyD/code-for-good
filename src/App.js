0cfdbd8c1ba7a3374842580e71f12229dd3b3939
Quincy Daniels <QUINCY.DANIELS@student.mec.cuny.edu>
2022-01-24 15:15:06 -0500

    Updated changes to Faq file

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import Links from './components/Nav/Links';
 import Nav from './components/Nav/Navbar';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {Faq} from './pages/Faq';
 
 function App() {
   return (
     <div className="App">
+      <Faq />
       <Nav />
       <Routes>
         {Links.map((element) => {
---
+++ src/pages/Faq.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+// function Faq () {
+//     return <h1>Faq Page</h1>
+// }
+
+export const Faq = () => <h1>Frequently asked Questions</h1>
+
+// export default Faq;
