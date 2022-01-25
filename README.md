3dd4c825dbb85574d398c05b41597e38a2492c65
allonahmed <allonahmed@mec.science>
2022-01-25 15:14:27 -0500

    fixed merge messages in package.json and README file

--- README.md
+++ README.md
@@ -1,13 +1,11 @@
 # Modules to Download
-npm i react-router-dom
-npm install @mui/material @emotion/react @emotion/styled
-<<<<<<< HEAD
 
-=======
-npm i react-parallax
-npm install @mui/icons-material
-npm i swiper
->>>>>>> main
+- npm i react-router-dom
+- npm install @mui/material @emotion/react @emotion/styled
+- npm i react-parallax
+- npm install @mui/icons-material
+- npm i swiper
+- npm i react-flippy
 
 # Getting Started with Create React App
 
--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import './App.css';
-import Links from './components/Nav/Links';
-import Nav from './components/Nav/Navbar';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import "./App.css";
+import Links from "./components/Nav/Links";
+import Nav from "./components/Nav/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
@@ -10,7 +10,11 @@ function App() {
       <Routes>
         {Links.map((element) => {
           return (
-            <Route path={element.link} element={element.component}/>
+            <Route
+              key={element}
+              path={element.link}
+              element={element.component}
+            />
           );
         })}
       </Routes>
